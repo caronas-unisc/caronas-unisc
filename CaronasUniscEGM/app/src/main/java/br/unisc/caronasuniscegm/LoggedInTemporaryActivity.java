cd8191d85a2caf9be04c88ad345e75395bd7e3df
Guilherme Sehn <ghsehn@gmail.com>
2015-10-03 16:38:16 -0300

    Logout movido para menu da activity

--- CaronasUniscEGM/app/src/main/java/br/unisc/caronasuniscegm/LoggedInTemporaryActivity.java
+++ CaronasUniscEGM/app/src/main/java/br/unisc/caronasuniscegm/LoggedInTemporaryActivity.java
@@ -41,27 +41,23 @@ public class LoggedInTemporaryActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_logged_in_temporary, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            logout();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void logout(View view) {
+    private void logout() {
         // Remove os dados do usuário das SharedPreferences
         SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key),
                 Context.MODE_PRIVATE);
