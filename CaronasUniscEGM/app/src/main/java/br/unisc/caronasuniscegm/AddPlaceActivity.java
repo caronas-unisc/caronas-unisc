e4963bdf6ba173802408808bbc97a074fb59f105
mfelipe_eisenkraemer <mfelipe@outlook.com>
2015-10-14 10:55:24 -0300

    Adicionado marker de destino para unisc no mapa de teste e a possibilidade de o usuario selecionar o marker de origem. Ainda falta calcular a rota entre os dois pontos.

--- CaronasUniscEGM/app/src/main/java/br/unisc/caronasuniscegm/AddPlaceActivity.java
+++ CaronasUniscEGM/app/src/main/java/br/unisc/caronasuniscegm/AddPlaceActivity.java
@@ -8,22 +8,40 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 public class AddPlaceActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private TextView markerText;
+    private LatLng center;
+    private LinearLayout markerLayout;
+    private Geocoder geocoder;
+    private List<Address> addresses;
+    private TextView Address;
+    private Marker mStartRideMarker;
+    private Marker mDestinationRideMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +72,12 @@ public class AddPlaceActivity extends FragmentActivity {
      * method in {@link #onResume()} to guarantee that it will be called.
      */
     private void setUpMapIfNeeded() {
+
+        markerText = (TextView) findViewById(R.id.locationMarkertext);
+        Address = (TextView) findViewById(R.id.adressText);
+        markerLayout = (LinearLayout) findViewById(R.id.locationMarker);
+
+
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
@@ -73,7 +97,30 @@ public class AddPlaceActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
+        // Centro de santa cruz do sul
+        LatLng latLong = new LatLng(-29.7032527, -52.4277339);
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLong).zoom(19f).tilt(70).build();
+
         mMap.setMyLocationEnabled(true);
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+        // Clears all the existing markers
+        mMap.clear();
+
+        // LatLong da unisc
+        LatLng latLng1 = new LatLng(-29.6987289,
+                -52.4372599);
+
+        mDestinationRideMarker = mMap.addMarker(new MarkerOptions()
+                .position(latLng1)
+                .title("Destination")
+                .snippet("")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.maps_marker_icon)));
+
+
         /*LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         try {
@@ -85,5 +132,121 @@ public class AddPlaceActivity extends FragmentActivity {
         } catch (SecurityException ex) {
 
         }*/
+
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+
+            @Override
+            public void onCameraChange(CameraPosition arg0) {
+
+                if( mStartRideMarker == null ){
+
+                    center = mMap.getCameraPosition().target;
+
+                    markerText.setText(" Set your Location ");
+                    //mMap.clear();
+                    markerLayout.setVisibility(View.VISIBLE);
+
+                    try {
+                        new GetLocationAsync(center.latitude, center.longitude)
+                                .execute();
+
+                    } catch (Exception e) {
+                    }
+                }
+
+            }
+        });
+
+        markerLayout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                try {
+
+                    LatLng latLng1 = new LatLng(center.latitude,
+                            center.longitude);
+
+                    mStartRideMarker = mMap.addMarker(new MarkerOptions()
+                            .position(latLng1)
+                            .title("Start")
+                            .snippet("")
+                            .icon(BitmapDescriptorFactory
+                                    .fromResource(R.drawable.maps_marker_icon)));
+                    mStartRideMarker.setDraggable(true);
+
+                    markerLayout.setVisibility(View.GONE);
+
+
+                } catch (Exception e) {
+                }
+
+            }
+        });
+    }
+
+    private class GetLocationAsync extends AsyncTask<String, Void, String> {
+
+        // boolean duplicateResponse;
+        double x, y;
+        StringBuilder str;
+
+        public GetLocationAsync(double latitude, double longitude) {
+            // TODO Auto-generated constructor stub
+
+            x = latitude;
+            y = longitude;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            Address.setText(" Getting location ");
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                geocoder = new Geocoder(AddPlaceActivity.this, Locale.ENGLISH);
+                addresses = geocoder.getFromLocation(x, y, 1);
+                str = new StringBuilder();
+                if (geocoder.isPresent()) {
+
+                    if( addresses.size() > 0 ){
+                        Address returnAddress = addresses.get(0);
+
+                        String localityString = returnAddress.getLocality();
+                        String city = returnAddress.getCountryName();
+                        String region_code = returnAddress.getCountryCode();
+                        String zipcode = returnAddress.getPostalCode();
+
+                        str.append(localityString + "");
+                        str.append(city + "" + region_code + "");
+                        str.append(zipcode + "");
+                    }
+
+
+                } else {
+                }
+            } catch (IOException e) {
+                Log.e("tag", e.getMessage());
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                Address.setText(addresses.get(0).getAddressLine(0)
+                        + addresses.get(0).getAddressLine(1) + " ");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+
+        }
     }
 }
