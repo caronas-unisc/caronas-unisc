6d30cfd3271409cad923591318d2b975b561372e
Guilherme Sehn <ghsehn@gmail.com>
2015-11-29 23:25:12 -0200

    fonte daora

--- CaronasUniscEGM/app/src/main/java/br/unisc/caronasuniscegm/MainActivity.java
+++ CaronasUniscEGM/app/src/main/java/br/unisc/caronasuniscegm/MainActivity.java
@@ -6,10 +6,13 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -35,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txt = (TextView) findViewById(R.id.caronas_unisc_title);
+        Typeface font = Typeface.createFromAsset(getAssets(), "Lato-Regular.ttf");
+        txt.setTypeface(font);
+
         setAlarm();
         checkSession();
     }
